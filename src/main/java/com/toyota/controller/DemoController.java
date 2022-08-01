34c615569b93b5b7423e4b3d00bf09930d3898c5
shaunoommen.oa <shaunoommen.alexander2018@vitstudent.ac.in>
2022-08-01 14:24:43 +0530

    added new method method1()

--- src/main/java/com/toyota/controller/DemoController.java
+++ src/main/java/com/toyota/controller/DemoController.java
@@ -1,8 +1,14 @@
 package com.toyota.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+	
+	@RequestMapping(value="/")
+	public String method1() {
+		return("hi");
+	}
 
 }
